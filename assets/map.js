b1c0b80c4f03736156e7443f4ba386e880768e71
Melinh Mai <melinh.mai@gmail.com>
2018-06-20 18:10:30 -0500

    update to the map

--- assets/map.js
+++ assets/map.js
@@ -1,40 +1,62 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCjw3ZOOzTjEiAs4FX0yVvnevh06UwoeMs",
+    authDomain: "fudmeh.firebaseapp.com",
+    databaseURL: "https://fudmeh.firebaseio.com",
+    projectId: "fudmeh",
+    storageBucket: "fudmeh.appspot.com",
+    messagingSenderId: "426120982640"
+};
+firebase.initializeApp(config);
 
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 29.7325483, lng: -95.5512395},
-          zoom: 15
-        });
-        infoWindow = new google.maps.InfoWindow;
+var database = firebase.database();
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
+var map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 29.7325483,
+            lng: -95.5512395
+        },
+        zoom: 15
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             };
+
+            database.ref('location').set({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            })
 
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
-          }, function() {
+        }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCibwKW2p8ZMMIaOBCHDPJcgNhlYyt1SU0&callback=initMap">
-    </script>
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+} 
+
+
+// src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCibwKW2p8ZMMIaOBCHDPJcgNhlYyt1SU0&callback=initMap" >
+ 
